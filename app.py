3b4d83cc7039bca1040136f178c8fc187875a58c
CamiloP121 <andresospino14@gmail.com>
2025-10-04 14:49:52 -0500

    start app

---
+++ app.py
@@ -0,0 +1,31 @@
+# API
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from modules.logger_config import logger
+
+print("||||| start configuration ai_app |||||")
+
+app = FastAPI(
+    title= "app-automatic-screening",
+    description="""
+    Aplicación screening automático NLP
+    """,
+    version="0.0.1"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir solicitudes desde cualquier origen
+    allow_credentials=True,  # Permitir el envío de credenciales (cookies, auth headers, etc.)
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permitir todos los headers
+)
+
+print("||||| end configuration ai_app |||||")
+
+@app.get("/lvz", summary="Verifica el estado del servidor", 
+         description="Este endpoint retorna el estado actual del servidor")
+def lvz():
+    logger.debug("Server run")
+    return {"status_server": "ok"}
---
+++ modules/logger_config.py
@@ -0,0 +1,25 @@
+# Sistema
+import os
+import logging
+
+mode = os.getenv("MODE", "dev")
+
+# Configure logging
+logging.getLogger('apscheduler.executors.default').propagate = False
+if mode == "dev":
+    logging.basicConfig(
+        format='"timestamp": %(asctime)s , %(message)s',
+        datefmt='"%d-%m-%Y %I:%M:%S %p"',
+        level=logging.DEBUG,
+    )
+else:
+    logging.basicConfig(
+        format='"timestamp": %(asctime)s , %(message)s',
+        datefmt='"%d-%m-%Y %I:%M:%S %p"',
+        level=logging.INFO,
+    )
+
+logger = logging.getLogger(__name__)
+
+logger.debug("Modo de ejecución: %s", mode)
+logger.info("Inicializando la aplicación...")
