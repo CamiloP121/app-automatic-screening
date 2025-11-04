a8814762907733f5d22634c3b43013bf87a769f2
CamiloP121 <andresospino14@gmail.com>
2025-11-03 20:12:21 -0500

    add new columns

--- modules/models/research_managment/Research.py
+++ modules/models/research_managment/Research.py
@@ -8,15 +8,17 @@ from ...models import db
 import uuid
 import pandas as pd
 
+
 class Research(db.base):
     __tablename__ = "researches"
 
     id = Column(String, primary_key=True)  # Id único de la investigación
-    name = Column(String, unique=True, nullable=False)                  # Nombre de la investigación
-    description = Column(String)           # Descripción de la investigación
-    created_at = Column(DateTime, default=datetime.now)   # Fecha de creación
-    methodology = Column(String)           # Metodología a utilizar
+    title = Column(String, unique=True, nullable=False) # Título de la investigación
+    type_research = Column(String, nullable=False)  # Tipo de investigación
+    methodology = Column(String)  # Metodología a utilizar
     criteria_inclusion = Column(String)    # Criterios de inclusión
+    created_at = Column(DateTime, default=datetime.now)   # Fecha de creación
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now) # Fecha de actualización
     is_active = Column(Boolean, default=True)  # Estado activo/inactivo
     is_test = Column(Boolean, default=False)  # Research de prueba o real
     step = Column(String)                # Estado actual dentro del pipeline
@@ -26,6 +28,7 @@ class Research(db.base):
     researcherOwner = relationship("Users", back_populates="researches")
 
     datasets = relationship("Datasets", back_populates="datasetOwner")
+    ai_labeled = relationship("AiLabeled", back_populates="ResearchOwner")
 
     @classmethod
     def add(cls, dict_new):
@@ -45,78 +48,34 @@ class Research(db.base):
         """Actualiza un Research existente según su id."""
         try:
             research = db.session.query(cls).filter_by(id=id).first()
-
             if research is None:
                 raise Exception(f"Research with id {id} not found.")
-
+            dict_update["updated_at"] = datetime.now()
             for key, value in dict_update.items():
                 if hasattr(research, key):
                     setattr(research, key, value)
                 else:
                     print(f"Attribute {key} does not exist on the Research model.")
-
             # db.session.commit()
             return research
         except Exception as e:
             print("Error ", e)
             # db.session.rollback()
             raise Exception("Error updating Research")
-
-    @classmethod
-    def deactivate(cls, id):
-        """Desactiva investigación (is_active=False) según su nickname."""
-        try:
-            user = db.session.query(cls).filter_by(id=id).first()
-            if user is None:
-                raise Exception(f"User with id {id} not found.")
-
-            user.is_active = False
-            # db.session.commit()  # Activa esta línea si quieres que se guarde de inmediato
-            return user
-        except Exception as e:
-            print("Error ", e)
-            # db.session.rollback()
-            raise Exception("Error deactivating Research")
-
-    @classmethod
-    def delete(cls, id):
-        """Elimina un Research por su id."""
-        try:
-            research = db.session.query(cls).filter_by(id=id).first()
-
-            if research is None:
-                raise Exception(f"Research with id {id} not found.")
-
-            db.session.delete(research)
-            # db.session.commit()
-        except Exception as e:
-            print("Error ", e)
-            # db.session.rollback()
-            raise Exception("Error deleting Research")
-
-    @classmethod
-    def id_exists(cls, id):
-        """Verifica si existe un Research con el id dado."""
-        return db.session.query(cls).filter_by(id=id).first() is not None
-
-    @classmethod
-    def get_id(cls, id):
-        """Obtiene un Research por su id."""
-        return db.session.query(cls).filter_by(id=id).first()
-
+        
     @classmethod
     def generate_unique_id(cls):
-        """Genera un id único no usado en la tabla Research."""
+        """Genera un id único no usado en la tabla Dataset."""
         while True:
             new_id = str(uuid.uuid4())
             existing = db.session.query(cls).filter_by(id=new_id).first()
             if not existing:
                 return new_id
-    
+        
     @classmethod
-    def name_exists(cls, name):
-        """Verifica si existe un Research con el name dado."""
-        return db.session.query(cls).filter_by(name=name).first() is not None
+    def title_exists(cls, title):
+        """Verifica si ya existe una investigación con el título dado."""
+        return db.session.query(cls).filter_by(title=title).first() is not None
 
     @classmethod
     def get_by_owner(cls, username):
@@ -128,11 +87,12 @@ class Research(db.base):
                 r_dict[r.researcherOwnerId] = []
             r_dict[r.researcherOwnerId].append({
                 "id": r.id,
-                "name": r.name,
-                "description": r.description,
-                "created_at": r.created_at,
+                "title": r.title,
+                "type_research": r.type_research,
                 "methodology": r.methodology,
                 "criteria_inclusion": r.criteria_inclusion.split("|&|") if r.criteria_inclusion else [],
+                "created_at": r.created_at,
+                "updated_at": r.updated_at,
                 "is_active": r.is_active,
                 "is_test": r.is_test,
                 "step": r.step
@@ -140,8 +100,13 @@ class Research(db.base):
         return r_dict
 
     @classmethod
+    def get_id(cls, id):
+        """Obtiene un Research por su id."""
+        return db.session.query(cls).filter_by(id=id).first()
+
+    @classmethod
     def get_all(cls):
-        """Devuelve un diccionario {id: name} de todas las investigaciones."""
+        """Devuelve un diccionario {id: title} de todas las investigaciones."""
         resultados = db.session.query(cls).all()
         if not resultados:
             return None
@@ -151,11 +116,12 @@ class Research(db.base):
                 r_dict[r.researcherOwnerId] = []
             r_dict[r.researcherOwnerId].append({
                 "id": r.id,
-                "name": r.name,
-                "description": r.description,
-                "created_at": r.created_at,
+                "title": r.title,
+                "type_research": r.type_research,
                 "methodology": r.methodology,
                 "criteria_inclusion": r.criteria_inclusion.split("|&|") if r.criteria_inclusion else [],
+                "created_at": r.created_at,
+                "updated_at": r.updated_at,
                 "is_active": r.is_active,
                 "is_test": r.is_test,
                 "step": r.step
