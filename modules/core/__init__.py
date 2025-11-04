9f7bfba31162b87a3cf138b6cca2bf8c7548f4f6
CamiloP121 <andresospino14@gmail.com>
2025-11-04 11:45:36 -0500

    add init

--- modules/core/__init__.py
+++ modules/core/__init__.py
@@ -2,3 +2,5 @@ from .embedding import Embedding
 from .retrievers import SimpleRetriever
 from .vectorestore_pg import VectorStorePG
 from .chat_llm import ChatLLM
+from .lg_model import LogicticRegretionAdaptative
+from .vectorizer import Vectorizer
